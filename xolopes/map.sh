c7046d150139615c7501bf29025a09ded71b3184
jjconti <jjconti@gmail.com>
2014-06-08 13:39:56 -0300

    More margin

--- xolopes/map.sh
+++ xolopes/map.sh
@@ -1,5 +1,5 @@
 python getDot.py
-dot -Tpdf -Gsize=5.8,8.3 chunks.txt -o chunks.pdf
+dot -Tpdf -Gsize=5.8,8.3 -Gmargin=0.8 chunks.txt -o chunks.pdf
 dot -Tsvg -Gsize=5.8,8.3 chunks.txt -o chunks.svg
 dot -Tpng -Gsize=5.8,8.3 chunks.txt -o chunks.png
 dot -Tps  -Gsize=5.8,8.3 chunks.txt -o chunks.ps
